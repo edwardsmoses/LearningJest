faf06c5e4551b60f858c64b6256405a284378f54
Edwards Moses <edwardsmoses3@gmail.com>
2021-02-19 10:51:53 +0100

    Testing Negative Result

--- __tests__/add.spec.js
+++ __tests__/add.spec.js
@@ -4,4 +4,10 @@ describe('add', () => {
     it('should add two numbers', () => {
         expect(add(1,2)).toBe(3);
     });
+
+    it('testing add - result should be negative', () => {
+        const actualResult = add(-2,1);
+        expect(actualResult).toBe(-1);
+    })
+
 });
