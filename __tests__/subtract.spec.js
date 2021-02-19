3cfd03061f5e4e28a3fc55d7b59a7b00a86ddfcb
Edwards Moses <edwardsmoses3@gmail.com>
2021-02-19 10:49:39 +0100

    ran subtract test sucessfully

--- __tests__/subtract.spec.js
+++ __tests__/subtract.spec.js
@@ -1,9 +1,7 @@
-import { expect, it } from "@jest/globals";
 const subtract = require("../subtract");
 
 describe("subtract", () => {
   it("should subtract two numbers", () => {
-    const actual = subtract(5, 3);
-    expect(actual).toBe(2);
+    expect(subtract(5,2)).toBe(3);
   });
 });
