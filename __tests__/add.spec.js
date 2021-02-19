7e81b4f2af717729d885f205f4ede17489b13575
Edwards Moses <edwardsmoses3@gmail.com>
2021-02-19 10:52:33 +0100

    Added a skipped test

--- __tests__/add.spec.js
+++ __tests__/add.spec.js
@@ -1,13 +1,17 @@
-const add = require('../add');
+const add = require("../add");
 
-describe('add', () => {
-    it('should add two numbers', () => {
-        expect(add(1,2)).toBe(3);
-    });
+describe("add", () => {
+  it("should add two numbers", () => {
+    expect(add(1, 2)).toBe(3);
+  });
 
-    it('testing add - result should be negative', () => {
-        const actualResult = add(-2,1);
-        expect(actualResult).toBe(-1);
-    })
+  it("testing add - result should be negative", () => {
+    const actualResult = add(-2, 1);
+    expect(actualResult).toBe(-1);
+  });
 
+  it.skip("testing add", () => {
+    const actualResult = add(1, 3);
+    expect(actualResult).toBe(4);
+  });
 });
